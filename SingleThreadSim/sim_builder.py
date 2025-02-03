Fixed fake all_reduce gradient strategy. Also fixed state dict loading.

SingleThreadSim/sim_builder.py
@@ -51,7 +51,8 @@ class SingleThreadSimBuilder:
                                               torch.device(self.config.device), 
                                               rank, 
                                               self.logger,
-                                              self.communication_handler))
+                                              self.communication_handler,
+                                              state_dict=None if rank == 0 else self.train_nodes[0].model.state_dict()))
 
         while self.local_step < self.max_steps:
             # if self.local_step % self.config.eval_interval == 0:

experiments/nanogpt_singlethread.py
@@ -61,7 +61,7 @@ def main():
         train_dataset=train_dataset,
         val_dataset=val_dataset,
         batch_size=args.batch_size,
-        gradient_class=AllReduceGradient,
+        gradient_class=FakeAllReduceGradient,
         # gradient_class=NoCommunicationGradient,
         gradient_config=GradientConfig(
             optimizer_class=torch.optim.SGD,