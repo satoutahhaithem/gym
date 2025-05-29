d71a2e809bb202ff4bca73c3d81c8faa58ceced1
Matt Beton <matthew.beton@gmail.com>
2025-05-29 10:50:07 +0100

    Small changes to dataset.

--- DistributedSim/example/nanogpt/download_dataset.py
+++ DistributedSim/example/nanogpt/download_dataset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import argparse
-from dataset.dataset import get_dataset
+from dataset import get_dataset
 
 def main():
   parser = argparse.ArgumentParser(description="Download dataset files for DistributedSim")
@@ -8,12 +8,14 @@ def main():
                       help="Dataset identifier (default: owt)")
   parser.add_argument("--proportion", type=float, default=1.0,
                       help="Proportion of dataset to download (0.0-1.0)")
+  parser.add_argument("--max_workers", type=int, default=8,
+                      help="Number of threads to use for downloading (default: 8)")
   
   args = parser.parse_args()
   
   # Call get_dataset which will download and cache the data
   # We discard the returned dataset since we only want to download
-  _, _ = get_dataset(args.dataset, 0, args.proportion)
+  _, _ = get_dataset(args.dataset, 0, args.proportion, max_workers=args.max_workers)
   
   print("Dataset download complete!")
 
